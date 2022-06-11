499a3095839db785a4d37a441509394778350b06
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2022-06-11 10:25:20 +0200

    Fix missing variable and type error

--- src/main.py
+++ src/main.py
@@ -154,7 +154,7 @@ def main(args):
 						if args.colorspace is True:
 							im = im.convert(colour_space)
 						new_image_path = image_path.rsplit('.', 1)[0] + '.jpg'
-						im.save(new_image_path, dpi=(DPI,DPI), quality=args.quality, optimize=args.optimize)
+						im.save(new_image_path, dpi=(args.dpi, args.dpi), quality=args.quality, optimize=args.optimize)
 						os.remove(image_path)
 					else:
 						if args.colorspace is True:
@@ -162,7 +162,7 @@ def main(args):
 						im.save(image_path, dpi=(args.dpi, args.dpi), quality=args.quality, optimize=args.optimize)
 						new_image_path = image_path
 					img = Image.open(new_image_path)
-					img.thumbnail(args.size, args.size, Image.ANTIALIAS)
+					img.thumbnail((args.size, args.size), Image.ANTIALIAS)
 					img.save(new_image_path)
 					count+=1
 				

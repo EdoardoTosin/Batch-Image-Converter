b5903300a776deb2735c65b064d9a2984aaa2618
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2022-06-13 22:08:07 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from colorama import Fore, Back, Style
 __author__		= "Edoardo Tosin"
 __copyright__	= "Copyright (C) 2021 Edoardo Tosin"
 __credits__		= "Edoardo Tosin"
-__license__		= "GPL"
+__license__		= "GPL-3.0"
 __version__		= "1.0.0"
 
 colorama.init(autoreset=True)

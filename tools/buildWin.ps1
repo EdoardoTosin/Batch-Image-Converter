00f400ff8dd38a084b3e1a00c41d20508c540ea0
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2023-03-27 17:42:34 +0200

    Fix build script

--- tools/buildWin.ps1
+++ tools/buildWin.ps1
@@ -1,1 +1,4 @@
-python -m PyInstaller src\main.py --onefile --distpath ./ --icon=doc\logo.png -n="Batch-Image-Converter-Win64"
+python -m pip install --user pyinstaller_versionfile
+python -m pip install --user PyInstaller
+python tools\versionfile_generator.py
+python -m PyInstaller .\src\main.py --onefile --distpath ./ --icon=.\doc\logo.png -n="Batch-Image-Converter-Win64" --version-file versionfile.txt

b24246d1634735e2d3151dc36dd0bfc2ab633d8d
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2023-05-22 13:19:27 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ __author__      = "Edoardo Tosin"
 __copyright__   = "Copyright (C) 2022-23 Edoardo Tosin"
 __credits__     = "Edoardo Tosin"
 __license__     = "GPL-3.0"
-__version__     = "1.2.6"
+__version__     = "1.2.5"
 
 colorama.init(autoreset=True)
 

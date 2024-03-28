adde53c8f345d188978792b8de1df3fc0337334c
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2024-03-28 20:27:56 +0100

    chore: Update copyright year to 2022-24

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ from packaging import version
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 __author__      = "Edoardo Tosin"
-__copyright__   = "Copyright (C) 2022-23 Edoardo Tosin"
+__copyright__   = "Copyright (C) 2022-24 Edoardo Tosin"
 __credits__     = "Edoardo Tosin"
 __license__     = "GPL-3.0"
 __version__     = "1.3.1"

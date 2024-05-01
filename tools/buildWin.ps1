e11b4646cfe6ceb990160d5dbed6761015a18189
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2024-05-01 18:42:51 +0200

    Update buildWin.ps1

--- tools/buildWin.ps1
+++ tools/buildWin.ps1
@@ -1,5 +1,5 @@
+python -m pip install --user -r requirements-build.txt
 python -m pip install --user -r requirements.txt
 python -m pip install --user pyinstaller_versionfile
-python -m pip install --user PyInstaller
 python tools\versionfile_generator.py
 python -m PyInstaller .\src\main.py --onefile --distpath ./ --icon=.\assets\logo.png -n="Batch-Image-Converter-Win64" --version-file versionfile.txt
